6c2029d8a83408c58b24634ebd65c22db430c5b8
holdn2 <yoocy01@gmail.com>
2025-03-11 20:31:41 +0900

    test: 위치 가져오기 테스트트

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,50 @@
 // 홈 페이지
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  // 위치 불러오기 연습용
+  interface Location {
+    latitude: number;
+    longitude: number;
+  }
+
+  const [location, setLocation] = useState<Location | null>(null);
+  const getLocation = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position: GeolocationPosition) => {
+          setLocation({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          });
+        },
+        (error: GeolocationPositionError) => {
+          console.error("Error getting location:", error.message);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  };
+  useEffect(() => {
+    if (location !== null) {
+      alert(`위치 : ${location?.latitude},${location?.longitude}`);
+    }
+  }, [location]);
+
   const navigate = useNavigate();
+
   useEffect(() => {
     navigate("/login");
+    getLocation();
   }, []);
-  return <div>Home</div>;
+  return (
+    <div>
+      위치 : {location?.latitude} / {location?.longitude}
+    </div>
+  );
 };
 
 export default Home;
