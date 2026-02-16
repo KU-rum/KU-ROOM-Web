b0bb3fd71adca5a896794b0d5c4cc0bce0525875
holdn2 <yoocy01@gmail.com>
2026-02-16 19:17:41 +0900

    code review: 쿼리키 수정

--- src/queryKey/department.ts
+++ src/queryKey/department.ts
@@ -1,5 +1,5 @@
 export const DEPARTMENT_QUERY_KEY = {
   COLLEGE: ["colleges"],
   DEPARTMENT: (college: string) => ["departments", college],
-  SEARCHED_DEPARTMENT: (search: string) => ["departments", search],
+  SEARCHED_DEPARTMENT: (search: string) => ["searched-departments", search],
 } as const;
